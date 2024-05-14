e24f1de387b9f6b7b7a0873b0bff1454c346027c
kana2022 <kkanat94@gmail.com>
2024-05-14 13:56:16 +0600

    1

--- PAMobile/App.xaml.cs
+++ PAMobile/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace PAMobile
+﻿using Foundation;
+using UIKit;
+
+namespace PAMobile
 {
     public partial class App : Application
     {
@@ -13,8 +16,11 @@
 #if __ANDROID__
                 handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
 #elif __IOS__
-            handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
-            handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+                handler.PlatformView.BackgroundColor = UIKit.UIColor.Clear;
+                handler.PlatformView.BorderStyle = UIKit.UITextBorderStyle.None;
+                var attributedString = new NSMutableAttributedString(handler.PlatformView.Text ?? "****");
+                attributedString.AddAttribute(UIStringAttributeKey.KerningAdjustment, NSNumber.FromFloat(15), new NSRange(0, handler.PlatformView.Text.Length));
+                handler.PlatformView.AttributedText = attributedString;
 #endif
             });
         }
