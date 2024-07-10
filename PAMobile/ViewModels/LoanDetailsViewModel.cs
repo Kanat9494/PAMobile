3e3387025725ec4470a5195f6df9880886feb9cd
kana2022 <kkanat94@gmail.com>
2024-07-10 09:36:29 +0600

    1

--- PAMobile/ViewModels/LoanDetailsViewModel.cs
+++ PAMobile/ViewModels/LoanDetailsViewModel.cs
@@ -235,7 +235,9 @@ internal class LoanDetailsViewModel : BaseViewModel, IQueryAttributable
                         //{
                         //    File = new ReadOnlyFile(filePath)
                         //});
-                        await Shell.Current.GoToAsync($"{nameof(PdfPage)}?{nameof(PdfViewModel.FilePath)}={filePath}");
+                        //await Shell.Current.GoToAsync($"{nameof(PdfPage)}?{nameof(PdfViewModel.FilePath)}={filePath}");
+                        Uri uri = new Uri($"file://{filePath}");
+                        await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
                     }
                     catch
                     {
