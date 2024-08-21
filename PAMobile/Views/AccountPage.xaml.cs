fa90ce1d1d9d4b532b3155e08aff2247e91ff976
kana2022 <kkanat94@gmail.com>
2024-08-21 10:11:15 +0600

    1.0.29

--- PAMobile/Views/AccountPage.xaml.cs
+++ PAMobile/Views/AccountPage.xaml.cs
@@ -8,7 +8,7 @@ public partial class AccountPage : ContentPage
 		InitializeComponent();
 
 		//GenerateUI();
-		//versionText.Text = $"Версия {AppInfo.VersionString}";
+		versionText.Text = versionText.Text + AppInfo.VersionString;
 
         BindingContext = _viewModel = new AccountViewModel();
 	}
